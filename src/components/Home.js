89527aab24c539466c6a7a542c7d29d96488650c
oshanedesign <oshane.design@gmail.com>
2022-05-16 16:23:02 -0500

    route setup

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => (
   <div className="home">
-    <h1>Hello World</h1>
+    <h1>Hello Home</h1>
   </div>
 );
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import { BiMicrophone } from 'react-icons/bi';
+import { FiSettings } from 'react-icons/fi';
+import '../App.css'
 const NavBar = () => (
   <div className="nav">
     <ul id="navList">
-      <li><Link id="anchora" to="/">Home</Link></li>
-      <li><Link id="anchorb" to="/Display">Display</Link></li>
+      <li><Link id="anchora" to="/">{"<"}2022</Link></li>
+      <li><Link id="anchorb" to="/Display">Covid Cases</Link></li>
     </ul>
+    <div className="logoDiv">
+      <BiMicrophone className="microphone" />
+      <FiSettings />
+    </div>
   </div>
 );
 
