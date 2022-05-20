790b6880a1ac62f9286ba730b3fa36ab9f8fa112
oshanedesign <oshane.design@gmail.com>
2022-05-20 18:24:30 -0500

    COmpleted linter errors

--- src/components/App.js
+++ src/components/App.js
@@ -17,7 +17,7 @@ function App() {
     dispatch(setInitialData());
   }, []);
   return (
-      <div className="App">
+    <Router className="App">
       <div className="contain">
         <NavBar />
         <Routes>
@@ -25,7 +25,7 @@ function App() {
           <Route path="/country/:id" element={<Display />} />
         </Routes>
       </div>
-    </div>
+    </Router>
   );
 }
 
