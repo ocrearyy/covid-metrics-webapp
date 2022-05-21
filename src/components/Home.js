fbff83ec6598932a55f7feb8eee7aec262badc03
oshanedesign <oshane.design@gmail.com>
2022-05-20 22:58:44 -0500

    final edits made

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,23 +3,27 @@ import { RiFirstAidKitLine } from 'react-icons/ri';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import thunkFunction from '../redux/API';
+import Search from './Search';
 
 const Home = () => {
   const country = useSelector((store) => store);
   thunkFunction();
   return (
-    <div className="home">
-      {country.map((country) => (
-        <Link to={`/country/${country.id}`} className="outer" key={country.id}>
-          <div className="row">
-            <div className="item">
-              <RiFirstAidKitLine className="first-aid" />
-              <h2 className="country">{country.id.toUpperCase()}</h2>
-              <button type="button" className="btn">View Details</button>
+    <div>
+      <Search />
+      <div className="home">
+        {country.map((country) => (
+          <Link to={`/country/${country.id}`} className="outer" key={country.id}>
+            <div className="row">
+              <div className="item">
+                <RiFirstAidKitLine className="first-aid" />
+                <h2 className="country">{country.id.toUpperCase()}</h2>
+                <button type="button" className="btn">View Details</button>
+              </div>
             </div>
-          </div>
-        </Link>
-      ))}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
